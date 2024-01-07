bd150feb122ba321353db540f97ac1481e494dd1
mahinjaman01 <mahinjaman434@gmail.com>
2024-01-08 00:22:10 +0600

    Mad Libs Game in Python

---
+++ MadLibsGame.py
@@ -0,0 +1,9 @@
+name = input("Enter Your Name :");
+fatherName = input("Enter Your Father Name :");
+motherName = input("Enter Your Mother Name :");
+study = input("Which class You Read In ?");
+relationship = input("Your Relationship Status?");
+
+
+print(f"My Panda name is {name}. My pandas father name is {fatherName}. My pandas mother name is {motherName}. My Panda read in {study}."
+      f"My panda is {relationship}");
